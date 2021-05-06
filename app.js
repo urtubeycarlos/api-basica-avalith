aca5406612c6d87b7ce0b0a339d024cb50a74b69
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-06 14:38:41 -0300

    updated routes

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const session = require('express-session');
+const formidableMiddleware = require('express-formidable');
 const md5 = require('md5');
+const authMiddleware = require('./middlewares/auth')
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(session({}));
+/* app.use(session({})); */
+app.use(formidableMiddleware())
 
-const autRouter = require('./routes/auth');
+const authRouter = require('./routes/auth');
 const careerRouter = require('./routes/career');
-app.use('/', autRouter);
+app.use('/', authRouter);
 app.use('/career', careerRouter);
 
 app.listen(port, () => {
