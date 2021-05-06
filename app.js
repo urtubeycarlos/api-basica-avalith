561de3640d3ff2e0e4557f654c2ade1c65629cbf
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-06 11:06:55 -0300

    added routers

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const md5 = require('md5');
 const port = process.env.port;
 const app = express();
 
+const autRouter = require('./routes/auth');
+const careerRouter = require('./routes/career');
+app.use('/', autRouter);
+app.use('/career', careerRouter);
+
 app.listen(port, () => {
     console.log(`Server listening in port: ${port}`);
 })
