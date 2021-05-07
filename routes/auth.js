2e6ca78d50201cd5a71e8c5944b204bcdc29f449
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-07 09:47:26 -0300

    changed send() by json()

--- routes/auth.js
+++ routes/auth.js
@@ -5,16 +5,16 @@ const db = require('./../db');
 
 router.post('/login', (request, response) => {
     if (!request.fields.email || !request.fields.password)
-        response.status(400).send({status:400, logged:false, msg:'invalid body'});
+        response.status(400).json({status:400, logged:false, msg:'invalid body'});
     db.query('select * from user where email = ?', request.fields.email, (error, result) => {
         if( error )
             response.sendStatus(500);
         else {
             if( result[0].email == request.fields.email && result[0].password == md5(request.fields.password) ){
                 request.session.user = request.fields.email;
-                response.status(200).send({status:200, logged:true});
+                response.status(200).json({status:200, logged:true});
             } else
-                response.status(400).send({status:400, logged:false});
+                response.status(400).json({status:400, logged:false});
         }
     })
 });
@@ -26,45 +26,45 @@ router.post('/logout', (request, response) => {
 
 router.post('/signup', (request, response) => {
     if( !request.fields.email || !request.fields.password )
-        response.status(400).send({status:400, signup:false, msg:'invalid body'});
+        response.status(400).json({status:400, signup:false, msg:'invalid body'});
     db.query ('insert into user (email, password) select ?, MD5(?) from dual where not exists ( select * from user where email = ? )', [request.fields.email, request.fields.password, request.fields.email], (error, result) => {
         if( error )
             response.sendStatus(500);
         else {
             if( result.affectedRows == 0 )
-                response.status(400).send({status:400, signup:false, msg:'user already exists'});
+                response.status(400).json({status:400, signup:false, msg:'user already exists'});
             else
-                response.status(200).send({status:200, signup:true, msg:'user added successfully'});
+                response.status(200).json({status:200, signup:true, msg:'user added successfully'});
         }
     });
 });
 
 router.put('/changepassword', (request, response) => {
     if( !request.fields.email || !request.fields.password || !request.fields.newPassword )
-        response.status(400).send({status:400, password_changed:false, msg:'invalid body'});
+        response.status(400).json({status:400, password_changed:false, msg:'invalid body'});
     db.query('update user set password = MD5(?) where email = ? and password = MD5(?)', [request.fields.newPassword, request.fields.email, request.fields.password], (error, result) => {
         if( error )
             response.sendStatus(500);
         else {
             if( result.affectedRows == 0 )
-                response.status(400).send({status:400, password_changed:false, msg:'invalidad email or password'});
+                response.status(400).json({status:400, password_changed:false, msg:'invalidad email or password'});
             else
-                response.status(200).send({status:200, password_changed:true, msg:'password changed'});
+                response.status(200).json({status:200, password_changed:true, msg:'password changed'});
         }
     });
 });
 
 router.delete('/signdown', (request, response) => {
     if( !request.fields.email || !request.fields.password)
-        response.status(400).send({status:400, signdown:false, msg:'invalid body'});
+        response.status(400).json({status:400, signdown:false, msg:'invalid body'});
     db.query('delete from user where email = ? and password = MD5(?)', [request.fields.email, request.fields.password], (error, result) => {
         if( error )
             response.sendStatus(500);
         else {
             if( result.affectedRows == 0 )
-                response.status(400).send({status:400, signdown:false, msg:'invalidad email or password'});
+                response.status(400).json({status:400, signdown:false, msg:'invalidad email or password'});
             else
-                response.status(200).send({status:200, signdown:false, msg:'user eliminated succesfully'});
+                response.status(200).json({status:200, signdown:false, msg:'user eliminated succesfully'});
         }
     })
 });
