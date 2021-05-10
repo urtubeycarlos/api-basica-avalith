7d48fb7fa58edcbe13f9c58ef4ea3216f0e9b247
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-10 17:19:08 -0300

    removed body-parser because not working

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const session = require('express-session');
 const formidableMiddleware = require('express-formidable');
-const bodyParser = require('body-parser');
 const authMiddleware = require('./middlewares/auth');
 const authRouter = require('./routes/auth');
 const careerRouter = require('./routes/career');
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(session({ secret: 'somevalue' }));
 app.use(formidableMiddleware());
 app.use('/', authRouter);
-/* app.use('/career', authMiddleware, careerRouter); */
+app.use('/career', authMiddleware, careerRouter);
 app.use('/career', careerRouter);
 
 app.listen(port, () => {
