6af9b519c3bd13b0c2621ec52f9c5898c170e134
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-10 15:12:44 -0300

    changed insert on post career

--- routes/career.js
+++ routes/career.js
@@ -4,7 +4,7 @@ const db = require('../db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  db.query('select name, institute from career', (error, results) => {
+  db.query('select id, name, institute from career', (error, results) => {
     if (error) {
       return res.sendStatus(500);
     }
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  db.query('select name, institute from career where id = ?', req.params.id, (error, result) => {
+  db.query('select id, name, institute from career where id = ?', req.params.id, (error, result) => {
     if (error) {
       return res.sendStatus(500);
     }
@@ -25,12 +25,12 @@ router.post('/', (req, res) => {
   if (!req.fields.name || !req.fields.institute) {
     return res.status(400).send({ status: 400, added: false, msg: 'invalid body' });
   }
-  return db.query('insert into career (name, institute) values(?, ?)', [req.fields.name, req.fields.institute], (error, result) => {
+  return db.query('insert into career (name, institute) values(?, ?)', [req.fields.name, req.fields.institute], (error) => {
     if (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        return res.status(400).send({ status: 400, added: false, msg: 'career already exists' });
+      }
       return res.sendStatus(500);
-    }
-    if (result.affectedRows === 0) {
-      return res.status(400).send({ status: 400, added: false, msg: 'career already exists' });
     }
     return res.status(200).send({ status: 200, added: true, msg: 'career added succesfully' });
   });
