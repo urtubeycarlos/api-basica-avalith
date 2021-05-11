dc0ff79dc77be7b30220cbc437aaf48431718a68
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-11 17:53:48 -0300

    changed expires by expire

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     database: 'university',
   },
   auth: {
-    expires: '1h',
+    expire: '1h',
     algorithm: 'ES256',
     privateKey: 'U-V&Qw;4,d^jbjN',
     passphrase: 'pu(HHND/E-mK//2',
