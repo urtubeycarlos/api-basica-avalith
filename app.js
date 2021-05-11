74124ee7fe2b4bd512d7dad1e5fed00608a78085
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-11 15:24:50 -0300

    removed express-formidable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const formidableMiddleware = require('express-formidable');
+/* const formidableMiddleware = require('express-formidable'); */
 const authMiddleware = require('./middlewares/auth');
 const authRouter = require('./routes/auth');
 const careerRouter = require('./routes/career');
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(session({ secret: 'somevalue' }));
-app.use(formidableMiddleware());
+/* app.use(formidableMiddleware()); */
 app.use('/', authRouter);
 app.use('/career', authMiddleware, careerRouter);
 app.use('/career', careerRouter);
