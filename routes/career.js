399eff89ba6decf6327abfec6b4d9c2a550f97ca
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-13 16:43:22 -0300

    changed db by career service

--- routes/career.js
+++ routes/career.js
@@ -1,51 +1,54 @@
 const express = require('express');
-const db = require('../db');
+const careerService = require('../services/careerService');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  db.query('select id, name, institute from career', (error, results) => {
-    if (error) {
-      return res.sendStatus(500);
-    }
-    return res.status(200).send(results);
-  });
-});
-
-router.get('/:id', (req, res) => {
-  db.query('select id, name, institute from career where id = ?', req.params.id, (error, result) => {
-    if (error) {
-      return res.sendStatus(500);
-    }
+router.get('/', async (req, res) => {
+  try {
+    const result = await careerService.getAll();
     return res.status(200).send(result);
-  });
-});
-
-router.post('/', (req, res) => {
-  if (!req.body.name || !req.body.institute) {
-    return res.status(400).send({ status: 400, added: false, msg: 'invalid body' });
+  } catch (error) {
+    return res.sendStatus(500);
   }
-  return db.query('insert into career (name, institute) values(?, ?)', [req.body.name, req.body.institute], (error) => {
-    if (error) {
-      if (error.code === 'ER_DUP_ENTRY') {
-        return res.status(400).send({ status: 400, added: false, msg: 'career already exists' });
-      }
-      return res.sendStatus(500);
-    }
-    return res.status(200).send({ status: 200, added: true, msg: 'career added succesfully' });
-  });
 });
 
-router.delete('/:id', (req, res) => {
-  db.query('delete from career where id = ?', req.params.id, (error, result) => {
-    if (error) {
-      return res.sendStatus(500);
+router.get('/:id', async (req, res) => {
+  try {
+    const result = await careerService.get(req.params.id);
+    return res.status(200).send(result);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    await careerService.insert(req.body);
+    return res.status(200).send({ added: true, msg: 'career added succesfully' });
+  } catch (error) {
+    if (error.code === 'ER_NOT_FIELD') {
+      return res.sendStatus(400);
     }
+    if (error.code === 'ER_DUP_ENTRY') {
+      return res.status(400).send({ added: false, msg: 'career already exists' });
+    }
+    return res.sendStatus(500);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const result = await careerService.remove(req.params.id);
     if (result.affectedRows === 0) {
-      return res.status(400).send({ status: 400, deleted: false, msg: 'career not exists' });
+      return res.status(400).send({ deleted: false, msg: 'career not exists' });
     }
-    return res.status(200).send({ status: 200, deleted: true, msg: 'career deleted successfully' });
-  });
+    return res.status(200).send({ deleted: true, msg: 'career deleted successfully' });
+  } catch (error) {
+    if (error.code === 'ER_NOT_ID') {
+      return res.sendStatus(400);
+    }
+    return res.sendStatus(500);
+  }
 });
 
 /* router.put('/:id', (req, res) => {
