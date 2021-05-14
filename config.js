dd10bc1e6ec4ce219775ed5f26ec36cf5fca5376
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-14 12:34:44 -0300

    added process.env.NODE_ENV

--- config.js
+++ config.js
@@ -1,16 +1,34 @@
+require('dotenv').config();
+
 const config = {
-  db: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'university',
+  test: {
+    db: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'university-test',
+    },
+    auth: {
+      expire: '1000',
+      algorithm: 'HS256', // HS256
+      privateKey: 'U-V&Qw;4,d^jbjN',
+      passphrase: 'pu(HHND/E-mK//2',
+    },
   },
-  auth: {
-    expire: '1h',
-    algorithm: 'HS256', // HS256
-    privateKey: 'U-V&Qw;4,d^jbjN',
-    passphrase: 'pu(HHND/E-mK//2',
+  dev: {
+    db: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'university',
+    },
+    auth: {
+      expire: '1h',
+      algorithm: 'HS256', // HS256
+      privateKey: 'U-V&Qw;4,d^jbjN',
+      passphrase: 'pu(HHND/E-mK//2',
+    },
   },
 };
-
-module.exports = config;
+console.log(process.env.NODE_ENV);
+module.exports = config[process.env.NODE_ENV];
