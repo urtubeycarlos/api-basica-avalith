52232277a7d61bed9ae0a6b0177ad1acafdc6d73
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-17 16:14:54 -0300

    added careerService border cases tests

--- test/services/careerService.test.js
+++ test/services/careerService.test.js
@@ -37,7 +37,7 @@ describe('Testing career Service', () => {
   describe('main methods', () => {
     it('getAll', async () => {
       const result = await careerService.getAll();
-      assert.equal(result.length, 3);
+      assert.strictEqual(result.length, 3);
     });
     it('get', async () => {
       const dbContent = await careerService.getAll();
@@ -51,13 +51,107 @@ describe('Testing career Service', () => {
       };
       await careerService.insert(newCareer);
       const result = await careerService.getAll();
-      assert.equal(result.length, 4);
+      assert.strictEqual(result.length, 4);
     });
     it('remove', async () => {
       const dbContent = await careerService.getAll();
       await careerService.remove(dbContent[0].id);
       const result = await careerService.getAll();
-      assert.equal(result.length, 2);
+      assert.strictEqual(result.length, 2);
+    });
+  });
+  describe('border cases', () => {
+    describe('getAll', () => {
+      it('empty result', async () => {
+        const dbContent = await careerService.getAll();
+        for (let i = 0; i < dbContent.length; i += 1) {
+          const career = dbContent[i];
+          await careerService.remove(career.id);
+        }
+        const result = await careerService.getAll();
+        assert.strictEqual(result.length, 0);
+      });
+    });
+    describe('get', () => {
+      it('null id', async () => {
+        try {
+          await careerService.get(null);
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_ID');
+        }
+      });
+      it('undefined id', async () => {
+        try {
+          await careerService.get(undefined);
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_ID');
+        }
+      });
+      it('career not exists', async () => {
+        const dbContent = await careerService.getAll();
+        const nextId = dbContent[2].id + 1;
+        const inexistent = await careerService.get(nextId);
+        assert.deepEqual(inexistent, {});
+      });
+    });
+    describe('insert', () => {
+      it('empty', async () => {
+        try {
+          await careerService.insert({});
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_FIELD');
+        }
+      });
+      it('null', async () => {
+        try {
+          await careerService.insert(null);
+        } catch (error) {
+          assert.isTrue(error instanceof TypeError);
+        }
+      });
+      it('undefined', async () => {
+        try {
+          await careerService.insert(undefined);
+        } catch (error) {
+          assert.isTrue(error instanceof TypeError);
+        }
+      });
+      it('null values', async () => {
+        try {
+          await careerService.insert({ name: null, institute: null });
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_FIELD');
+        }
+      });
+      it('undefined values', async () => {
+        try {
+          await careerService.insert({ name: undefined, institute: undefined });
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_FIELD');
+        }
+      });
+      it('duplicated entry', async () => {
+        try {
+          await careerService.insert(fakeCareers[0]);
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_DUP_ENTRY');
+        }
+      });
+    });
+    describe('remove', () => {
+      it('career not exists', async () => {
+        const dbContent = await careerService.getAll();
+        const nextId = dbContent[2].id + 1;
+        const resultOfInexistent = await careerService.remove(nextId);
+        assert.strictEqual(resultOfInexistent.affectedRows, 0);
+      });
+      it('not id', async () => {
+        try {
+          await careerService.remove(null);
+        } catch (error) {
+          assert.strictEqual(error.code, 'ER_NOT_ID');
+        }
+      });
     });
   });
 });
